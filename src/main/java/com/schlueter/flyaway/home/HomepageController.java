77e2f798e04f77508490b978f6ed5475a53e5c2c
AlexS1986 <alexander.schlueter86@gmx.de>
2023-05-17 11:59:47 +0200

    Sprint 2: Refactoring and Securing /admin endpoints

---
+++ src/main/java/com/schlueter/flyaway/home/HomepageController.java
@@ -0,0 +1,14 @@
+package com.schlueter.flyaway;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomepageController {
+    @GetMapping("/")
+    public String getHomepage() {
+        return "homepage";
+    }
+}
---
+++ src/main/java/com/schlueter/flyaway/security/FlyAwaySecurityConfig.java
@@ -0,0 +1,2 @@
+package com.schlueter.flyaway.security;public class FlyAwaySecurityConfig {
+}
